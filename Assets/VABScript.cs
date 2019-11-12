187da9b9ea7b0746b29b28fe0c0eedade00b5730
Speakingevil <47955427+Speakingevil@users.noreply.github.com>
2019-11-12 01:27:43 +0000

    Build 1.1 of The Very Annoying Button

--- Assets/VABScript.cs
+++ Assets/VABScript.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class VABScript : MonoBehaviour {
@@ -28,6 +29,7 @@ public class VABScript : MonoBehaviour {
     private bool pressable;
     private bool movehatch;
     private bool submitted;
+    private bool twitchActive;
     private int buffer;
 
     //Logging
@@ -79,8 +81,22 @@ public class VABScript : MonoBehaviour {
         {
             r.material = arrowcol[1];
         }
+        GetComponent<KMBombModule>().OnActivate = OnActivate;
         StartCoroutine(Reset());
 	}
+
+    private void OnActivate()
+    {
+        if (TwitchPlaysActive)
+        {
+            twitchActive = true;
+        }
+        else
+        {
+            twitchActive = false;
+        }
+        Debug.LogFormat("[The Very Annoying Button #{0}] Twitch Plays mode: {1}", moduleId, TwitchPlaysActive);
+    }
 
     private IEnumerator Reset()
     {
@@ -104,7 +120,14 @@ public class VABScript : MonoBehaviour {
                 }
                 if (i != -1)
                 {
-                    yield return new WaitForSeconds(1);
+                    if (twitchActive)
+                    {
+                        yield return new WaitForSeconds(2);
+                    }
+                    else
+                    {
+                        yield return new WaitForSeconds(1);
+                    }
                 }
             }
             else
@@ -124,13 +147,13 @@ public class VABScript : MonoBehaviour {
                         {
                             GetComponent<KMBombModule>().HandleStrike();
                             screen.material = arrowcol[0];
-                            Debug.LogFormat("[The Very Annoying Button #{0}]Error: Button not pressed", moduleId);
+                            Debug.LogFormat("[The Very Annoying Button #{0}] Error: Button not pressed", moduleId);
                         }
                         else if (submitted == true && !valid[buttoncolour].Where(x => !record.Contains(x)).Contains(submission))
                         {
                             GetComponent<KMBombModule>().HandleStrike();
                             screen.material = arrowcol[0];
-                            Debug.LogFormat("[The Very Annoying Button #{0}]Error: Invalid submission", moduleId);
+                            Debug.LogFormat("[The Very Annoying Button #{0}] Error: Invalid submission", moduleId);
                         }
                         else if (submitted == true)
                         {
@@ -147,7 +170,7 @@ public class VABScript : MonoBehaviour {
                             else
                             {
                                 record[buttoncolour] = submission;
-                                Debug.LogFormat("[The Very Annoying Button #{0}]OK: The {2} button has recorded the value {1}", moduleId, submission, collog[buttoncolour]);
+                                Debug.LogFormat("[The Very Annoying Button #{0}] OK: The {2} button has recorded the value {1}", moduleId, submission, collog[buttoncolour]);
                                 Audio.PlaySoundAtTransform("InputCorrect", transform);
                             }
                         }
@@ -188,7 +211,7 @@ public class VABScript : MonoBehaviour {
                                     disp.color = new Color32(0, 0, 255, 255);
                                     break;
                             }
-                            Debug.LogFormat("[The Very Annoying Button #{0}]The button had turned {1} at {2}", moduleId, collog[buttoncolour], bomb.GetFormattedTime());
+                            Debug.LogFormat("[The Very Annoying Button #{0}] The button had turned {1} at {2}", moduleId, collog[buttoncolour], bomb.GetFormattedTime());
                         }
                         else
                         {
@@ -265,6 +288,76 @@ public class VABScript : MonoBehaviour {
             }
             arrows[9 - remaining].material = arrowcol[2];
             arrows[18 - remaining].material = arrowcol[2];
+        }
+    }
+
+    //twitch plays
+    bool TwitchPlaysActive;
+
+    private bool validCmd(string[] cmds)
+    {
+        string[] valids = { "1","2","3","4","5","6","7","8","9" };
+        for(int i = 1; i < cmds.Length; i++)
+        {
+            if (!valids.Contains(cmds[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<int> parseVals(string[] cmds)
+    {
+        List<int> ints = new List<int>();
+        for (int i = 1; i < cmds.Length; i++)
+        {
+            int temp = 0;
+            int.TryParse(cmds[i], out temp);
+            ints.Add(temp);
+        }
+        ints.Sort();
+        return ints;
+    }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press <#> (#)... [Press the button when the number of seconds remaining on the counter is <#> (or (#) and so on)] | Reference currently lit arrows for seconds remaining (1-9), actual seconds remaining is slightly extended in TP";
+    #pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        string[] parameters = command.Split(' ');
+        if (Regex.IsMatch(parameters[0], @"^\s*press\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            if(parameters.Length >= 2)
+            {
+                if(validCmd(parameters)){
+                    yield return null;
+                    List<int> parsedvals = parseVals(parameters);
+                    int waittime = 0;
+                    for(int i = parsedvals.Count-1; i >= 0; i--)
+                    {
+                        if(remaining > parsedvals.ElementAt(i))
+                        {
+                            waittime = parsedvals.ElementAt(i);
+                            break;
+                        }
+                    }
+                    while(remaining != waittime)
+                    {
+                        yield return "trycancel Button submission cancelled due to a cancel request.";
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                    if(waittime != 0)
+                    {
+                        button.OnInteract();
+                    }
+                    else
+                    {
+                        yield return "sendtochaterror Button times missed! Press command cancelled.";
+                    }
+                }
+            }
+            yield break;
         }
     }
 }
